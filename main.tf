1fc1ffdb6bc256e9e0f68759f142f8bc5cee6e3b
Nazeer <nazeer.deens@gmail.com>
2026-07-17 20:57:34 +0530

    Modified main.tf

--- main.tf
+++ main.tf
@@ -1,40 +1,8 @@
-module "ec2-dev" {
-  source = "./modules/ec2"
-
-  instance_name = "dev-${var.instance_name}"
-  instance_type = var.instance_type
+resource "aws_instance" "jenkinsins" {
   ami           = var.ami
-}
-
-module "ec2-model" {
-  source = "./modules/ec2"
-
-  instance_name = "model-${var.instance_name}"
   instance_type = var.instance_type
-  ami           = var.ami
+
+  tags = {
+    Name = var.instance_name
+  }
 }
-
-module "ec2-prod" {
-  source = "./modules/ec2"
-
-  instance_name = "prod-${var.instance_name}"
-  instance_type = var.instance_type
-  ami           = var.ami
-}
-
-module "ec2-qa" {
-  source = "./modules/ec2"
-
-  instance_name = "qa-${var.instance_name}"
-  instance_type = var.instance_type
-  ami           = var.ami
-}
-
-module "ec2-uat" {
-  source = "./modules/ec2"
-
-  instance_name = "uat-${var.instance_name}"
-  instance_type = var.instance_type
-  ami           = var.ami
-}
-
